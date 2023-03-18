fea166763b9018751513d4be621cbefc234accbe
ztclog <cece140607@163.com>
2023-03-18 21:04:46 +0800

    upload firefox user.js

---
+++ .config/user.js
@@ -0,0 +1,27 @@
+//
+user_pref("clipboard.autocopy",		false);
+user_pref("font.language.grou",		"zh-CN");
+user_pref("geo.enabled",		false);
+user_pref("pdfjs.disable",		true);
+user_pref("media.ffmpeg.vaapi.enable",	true);
+user_pref("media.video_stats.enabled",	false);
+user_pref("media.webspeech.recognition.enable",		false);
+user_pref("network.manage-offline-status",false);
+user_pref("browser.pocket.enabled",   	false);
+user_pref("extensions.pocket.enabled",	false);
+user_pref("plugins.click_to_play",	true);
+user_pref("extensions.systemAddon.update.enabled",	false);
+user_pref("browser.search.update",	false);
+user_pref("network.cookie.thirdparty.sessionOnly",	true);
+user_pref("browser.cache.offline.enable",		false);
+user_pref("browser.download.manager.retention",		0);
+user_pref("browser.formfill.enable",			true);
+user_pref("browser.helperApps.deleteTempFileOnExit",	true);
+user_pref("browser.pagethumbnails.capturing_disabled",	true);
+user_pref("browser.shell.shortcutFavicons",		false);
+user_pref("signon.management.page.fileImport.enabled",	true);
+user_pref("security.insecure_password.ui.enabled",	true);
+user_pref("layout.css.visited_links_enabled",		false);
+user_pref("browser.urlbar.autocomplete.enabled",	false);
+user_pref("browser.shell.checkDefaultBrowser",		false);
+user_pref("dom.security.https_only_mode",		true);
