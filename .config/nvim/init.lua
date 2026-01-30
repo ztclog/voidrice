315b2aaf43181ddb8e08a4969f09a79377cd16b5
ztclog <60066227+ztclog@users.noreply.github.com>
2026-01-31 00:50:11 +0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,24 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " " 
+vim.g.maplocalleader = "\\"
 vim.o.ic = true
 vim.o.nu = true
 vim.o.rnu = true
@@ -6,78 +27,54 @@ vim.o.title = true
 vim.o.cb = "unnamedplus"
 vim.o.encoding = "utf-8"
 vim.o.termguicolors = true
+vim.cmd[[
+hi Normal guibg=NONE ctermbg=NONE
+hi NormalFloat guibg=NONE ctermbg=NONE
+]]
 
-vim.g.mapleader = " " 
-
--- lazy.nvim plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-	"sbdchd/neoformat",
-	"tpope/vim-surround",
-	"tpope/vim-commentary",
-	"rcarriga/nvim-notify",
-	"neovim/nvim-lspconfig",
-	"kevinhwang91/nvim-hlslens",
-	"terryma/vim-multiple-cursors",
-	"nvim-treesitter/nvim-treesitter",
-	{
-		"junegunn/goyo.vim",
-		keys = {
-			{ "<leader>y", "<cmd>Goyo<CR>" },
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		"sbdchd/neoformat",
+		"stevearc/conform.nvim",
+		"mg979/vim-visual-multi",
+		{
+			'nvim-treesitter/nvim-treesitter',
+			lazy = false,
+			build = ':TSUpdate'
+		},
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+			keys = {
+				{
+					"<leader>?",
+					function()
+						require("which-key").show({ global = false })
+					end,
+					desc = "Buffer Local Keymaps (which-key)",
+				},
+			},
+		},
+		{
+			'numToStr/Comment.nvim',
+			opts = {
+				-- add any options here
+			},
+		},
+		{
+			'stevearc/conform.nvim',
+			opts = {},
 		},
 	},
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		keys = {
-			{ "<C-p>", "<cmd>MarkdownPreviewToggle<CR>" },
-		},
-		build = function() vim.fn["mkdp#util#install"]() end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		keys = {
-			{ "<leader>n", "<cmd>Neotree toggle<CR>" },
-		},
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		}
-	}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-require("hlslens").setup()
-
--- cargo install
-require'lspconfig'.harper_ls.setup{}
--- npm i -g 
-require'lspconfig'.astro.setup{}
--- pacman -S
-require'lspconfig'.ccls.setup{}
-require'lspconfig'.cssls.setup{}
-require'lspconfig'.vimls.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.lua_ls.setup{}
-require'lspconfig'.yamlls.setup{}
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
