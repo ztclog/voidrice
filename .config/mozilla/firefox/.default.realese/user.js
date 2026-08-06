ecf3990a6fc4dbafa3b8e4832a653b52d84d20ca
ztclog <60066227+ztclog@users.noreply.github.com>
2026-08-07 00:41:37 +0800

    Update user.js

--- .config/mozilla/firefox/.default.realese/user.js
+++ .config/mozilla/firefox/.default.realese/user.js
@@ -8,7 +8,6 @@ user_pref("font.name.monospace.zh-CN", "monospace");
 user_pref("font.name.sans-serif.zh-CN", "Noto Sans");
 
 user_pref("media.ffmpeg.vaapi.enable", true);
-user_pref("sidebar.visibility", "hide-sidebar");
 user_pref("widget.gtk.global-menu.enabled", false);		// 全局菜单 添加此项去除多于空白
 user_pref("widget.gtk.global-menu.wayland.enabled", true);	// 全局菜单
 user_perf("toolkit.legacyUserProfileCustomizations.stylesheets", true);
@@ -19,20 +18,3 @@ user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_perf("browser.urlbar.scotchBonnet.enableOverride", false);
 user_perf("browser.newtabpage.activity-stream.feeds.topsites", false);
 user_perf("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-
-user_pref("ultima.contextmenu.no.navigation.icons", true);
-user_pref("ultima.contextmenu.reduce.options", true);
-user_pref("ultima.disable.windowcontrols.button", true);
-user_pref("ultima.navbar.float", true);
-user_pref("ultima.navbar.position", "bottom");
-user_pref("ultima.navbar.theme.extensionspanel", true);
-user_pref("ultima.spacing.compact", true);
-user_pref("ultima.spacing.compact.tabs", true);
-user_pref("ultima.tabs.focus.blur", true);
-user_pref("ultima.tabs.tabCounter", true);
-user_pref("ultima.theme.icons", true);
-user_pref("ultima.urlbar.animate.open", true);
-user_pref("ultima.urlbar.centered", true);
-user_pref("ultima.xstyle.highlight.aboutconfig", true);
-user_pref("ultima.xstyle.private", false);
-user_pref("user.theme.fluent", true);
